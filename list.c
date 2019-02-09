0e2c9f64077fb493f9ff35630d50ff5ec31eb476
Cliff <tools@3csonline.com>
2019-02-09 18:05:12 +0000

    freelist function.

--- list.c
+++ list.c
@@ -7,56 +7,59 @@ typedef struct Node
     struct Node *next;
 } Node;
 
-void push(int, struct Node*);
-int pop(struct Node*);
-// int peek(struct Node*);
+void push(int, Node*);
+int pop(Node*);
+// int peek(Node*);
+void freeList(Node*);
 
 int main(void)
 {
     printf("Hello\n");
-    struct Node* head = NULL;
-    struct Node* second = NULL;
-    struct Node* third = NULL;
+    Node* head = NULL;
+    Node* second = NULL;
+    Node* third = NULL;
 
-    head = (struct Node*)malloc(sizeof(struct Node));
-    second = (struct Node*)malloc(sizeof(struct Node));
-    third = (struct Node*)malloc(sizeof(struct Node));
+    head = (Node*)malloc(sizeof(Node));
+    second = (Node*)malloc(sizeof(Node));
+    third = (Node*)malloc(sizeof(Node));
 
     head->data = 5;
-    head->next = second;
-    head->next->data = 7;
-    head->next->next = third;
-    head->next->next->data = 9;
-    head->next->next->next = NULL;
+    head->next = NULL;
+    // head->next = second;
+    // head->next->data = 7;
+    // head->next->next = third;
+    // head->next->next->data = 9;
+    // head->next->next->next = NULL;
     // second->data = 7;
     // second->next = third;
     // third->next = NULL;
     // third->data = 9
-    push(11, head);
+    // push(11, head);
     printf("%i\n", pop(head));
+    // freeList(head);
 }
 
-void push(int data, struct Node* head)
+void push(int data, Node* head)
 {
-    struct Node* trav = head;
+    Node* trav = head;
 
     while(trav->next != NULL)
     {
         trav = trav->next;
     }
 
-    // trav->next = (struct Node*)malloc(sizeof(struct Node))
+    // trav->next = (Node*)malloc(sizeof(Node))
     // trav->next->data = data
     // trav->next->next = NULL;
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->data = data;
     newNode->next = NULL;
     trav->next = newNode;
 }
 
-int pop(struct Node* head)
+int pop(Node* head)
 {
-    struct Node* trav = head;
+    Node* trav = head;
 
     // find the second to last node
     while (trav->next->next != NULL)
@@ -68,4 +71,23 @@ int pop(struct Node* head)
     free(trav->next);
     trav->next = NULL;
     return data;
+}
+
+void freeList(Node* root)
+{
+    Node* trav = root;
+    if (trav->next == NULL)
+    {
+        free(trav);
+        return;
+    }
+    if (trav->next->next == NULL)
+    {
+        free(trav->next);
+        free(trav);
+        return;
+    }
+    freeList(trav->next);
+    free(root);
+    return;
 }
